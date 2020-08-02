1424470cc3932190e5eb55c741de99f118d8ed00
jerryhanwei <jerryhanwei@163.com>
2020-08-02 08:28:51 +0800

    doc

--- mydoc/myplus.c
+++ mydoc/myplus.c
@@ -2,18 +2,18 @@
 
 #include "mpi.h"
 #include <stdio.h>
-
-typedef unsigned long long ULONG;
-ULONG m = 0;
+#include <math.h>
+#include <time.h>
+long m = 0;
 double getNewDistance(double x, double y)
 {
     return (x*x + y*y);
 }
 
 //  y=xn;
-ULONG getMultiNum(int x, int n)
+long getMultiNum(int x, int n)
 {
-    ULONG y = 1;
+    long y = 1;
     int i;
     for (i=0;i < n; i++)
     {
@@ -27,7 +27,7 @@ double getKeyRandomNumber(double px) {
 
     const  double a = 1664525;
     const  double c = 1013904223;
-    ULONG py = fmod((a * px + c), m);
+    long py = fmod((a * px + c), m);
 
     return 2 * (double)py / m - 1;
 
@@ -44,11 +44,9 @@ long getFallInCircleCount(int startI, int n) {
     {
         x = getKeyRandomNumber(i);
         y = getKeyRandomNumber(i - x);
-       // printf("x: %f  y: %f",x,y);
+
         if (getNewDistance(x, y) <= 1)
         {
-           // printf("中了 %d\n",i);
-
             circlePointCount++;
         }
 
@@ -58,21 +56,12 @@ long getFallInCircleCount(int startI, int n) {
 
 }
 
-int myplus(int startI,int n){
 
-    int sum =0;
-    int i;
-    for(i=startI;i<n;i++)
-    {
-        sum = sum + i;
-    }
-    return sum;
-}
 //always use argc and argv, as mpirun will pass the appropriate parms.
 int main(argc,argv)int argc;char *argv[];
 {
     m = getMultiNum(2,32);
-    int N,i,result,sum0,sum1,myid;
+    long N,i,numproc,myid;
     long allcount=0;
     long masterCount;
     long slaveCount;
@@ -84,6 +73,8 @@ int main(argc,argv)int argc;char *argv[];
 
     // Which node is this?
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Comm_size(MPI_COMM_WORLD, &numproc); //how many processors??
 
     // Do this block if master process
     if (myid == 0) {
@@ -91,33 +82,47 @@ int main(argc,argv)int argc;char *argv[];
         // Get the number from the user
         N=atoi(argv[1]);
 
-        // Master sends 'N' to slave
-        MPI_Send(&N, 1, MPI_INT, 1,0, MPI_COMM_WORLD);
+        for  (i=1;i<numproc;i++) {
+            MPI_Send(&N, 1, MPI_LONG, i,0, MPI_COMM_WORLD);
+        }
 
-        //sum0=myplus(0,N/2);
-         masterCount = getFallInCircleCount(0,N/2);
+
+         masterCount = getFallInCircleCount(0,N/numproc);
         printf(" 主线程 击中次数 ：%ld",masterCount);
-        // Master waits to receive 'sum1' from slave
-        MPI_Recv(&slaveCount, 1, MPI_LONG, 1,0, MPI_COMM_WORLD, &stat);
 
-        printf(" 主线程 接收奴隶线程 击中次数 ：%ld\n",slaveCount);
-        allcount = masterCount + slaveCount;
+        for (i=1;i<numproc;i++) {//receive from all nodes
+
+            MPI_Recv(&slaveCount, 1, MPI_LONG, i,0, MPI_COMM_WORLD, &Stat);
+
+            allcount = allcount + slaveCount;//adds the various sums
+
+            fprintf(stdout, "node=%ld: 目前 总计 %ld  当前进程击中点数： %ld\n", i, allcount, slaveCount);
+        }
+
+
+
+       // printf(" 主线程 接收奴隶线程 击中次数 ：%ld\n",slaveCount);
+        allcount = masterCount + allcount;
         double PI = (((double)allcount) / N) * 4;
-        printf("一共投掷：%d\n  击中了 -- %ld\n PI:---%f",N,allcount,PI);
+        printf("一共投掷：%ld\n  击中了 -- %ld\n PI:---%f",N,allcount,PI);
 
-        //fprintf(stdout,"投掷总次数：%d\n，共击中次数：%ld\n",N,allcount);
+
     }
 
         // Do this block if slave process
-    else if (myid == 1) {
+    else  {
 
         // Slave waits to receive 'N' from master
-        MPI_Recv(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);
+        MPI_Recv(&N, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD, &stat);
+        long oneSlaveCount = 0;
+        for(i=(N/numproc*myid)+1;i<=(N/numproc*(myid+1));i++){
+            oneSlaveCount = oneSlaveCount + getFallInCircleCount(i,(N / numproc * (myid + 1)));
+        }
 
-        slaveCount = getFallInCircleCount((N/2)+1,N);
-        printf(" 奴隶线程 击中次数 ：%ld",slaveCount);
+       // slaveCount = getFallInCircleCount((N/2)+1,N);
+        printf(" 奴隶线程 %ld 击中次数 ：%ld",myid,oneSlaveCount);
         // Slave sends 'sum1' to master
-        MPI_Send(&slaveCount, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&oneSlaveCount, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
